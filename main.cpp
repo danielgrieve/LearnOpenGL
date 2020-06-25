825e97e265158641ff60031730766bf07db1e42c
Daniel Grieve <daniel@danielgrieve.uk>
2020-06-25 08:04:13 +0100

    Create opengl context

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
-#include <iostream>
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <stdlib.h>
 
-void process_input(GLFWwindow *window);
+void framebuffer_size_callback(GLFWwindow*, int, int);
+void process_input(GLFWwindow*);
 
 int main() {
     if (!glfwInit()) {
@@ -14,8 +16,20 @@ int main() {
         return EXIT_FAILURE;
     }
 
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        return EXIT_FAILURE;
+    }
+
     while (!glfwWindowShouldClose(window)) {
         process_input(window);
+
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
@@ -23,10 +37,11 @@ int main() {
     return EXIT_SUCCESS;
 }
 
-    return 0;
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+    glViewport(0, 0, width, height);
 }
 
-void process_input(GLFWwindow *window) {
+void process_input(GLFWwindow* window) {
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
         glfwSetWindowShouldClose(window, true);
     }
